0e2023c967d453fb053245f63a427d2730636caa
phuongnam <nam@gmail.com>
2023-08-21 08:40:37 +0700

    test rebase

---
+++ app/Http/Controllers/TestRebaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestRebaseController extends Controller
+{
+    //
+    public function rebase(){
+        return "rebase";
+    }
+}
