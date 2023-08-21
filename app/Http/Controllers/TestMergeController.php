674ee27e41581c7eff45bba48e69d4be5a287e54
phuongnam <nam@gmail.com>
2023-08-21 08:45:24 +0700

    test merge

---
+++ app/Http/Controllers/TestMergeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestMergeController extends Controller
+{
+    //
+    public function merge(){
+        return "merge";
+    }
+}
