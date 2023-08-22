5add5bad309f0946ef40ff6edfdf11e99da47626
tunglx <ppnam150399@gmail.com>
2023-08-22 12:56:54 +0700

    new commit

--- app/Http/Controllers/TestMainController.php
+++ app/Http/Controllers/TestMainController.php
@@ -10,5 +10,14 @@ class TestMainController extends Controller
     public function abc(){
         return "abc";
     }
+    public function xyz(){
+        return "abc";
+    }
+    public function srgr(){
+        return "abc";
+    }
+    public function drdr(){
+        return "abc";
+    }
 
 }
