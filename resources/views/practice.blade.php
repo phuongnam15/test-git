040246b813d325f74e547638dba4790658b74fc9
phuongnam15 <ppnam150399@gmail.com>
2023-08-26 13:33:21 +0700

    1

---
+++ resources/views/practice.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        .toggle{
+            border: 1px solid;
+            width: 200px;
+            height: 20px;
+            margin-bottom: 5px;
+        }
+        .content{
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="toggle">
+        <p class="content">Content 1</p>
+    </div>
+    <div class="toggle">
+        <p class="content">Content 2</p>
+    </div>
+    <div class="toggle">
+        <p class="content">Content 3</p>
+    </div>
+</body>
+<script>
+    
+</script>
+</html>
