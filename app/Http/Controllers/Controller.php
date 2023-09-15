5cd03dceee966cd6a9d17bb56eb3623309470351
phuongnam15 <ppnam150399@gmail.com>
2023-09-15 20:58:05 +0700

    create helper

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Http\Controllers\TestInstanceOfController;
+use Carbon\Carbon;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-    public function xyz(){
-        return "xyz";
-    }
 }
